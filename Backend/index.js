aec040757b4c390930263ed03bf7183df59742eb
Daneesha Hansaka <daneesha.2003@hotmail.com>
2024-12-05 20:01:58 +0530

    Implemented Vendor and Customer worker thread management with TicketPool Integration
    - Implemented logic to start and manage vendor and customer worker threads in response to system initialization requests.
    - Introduced a shared Ticket Pool instance to handle ticket addition and removal, allowing synchronized interaction between vendors and customers.
    - Set up message handlers for both vendor and customer workers to process ticket-related actions (Generate Tickets and Purchase Ticket).
    - Added error and exit event listeners for robust thread error handling and monitoring.
    - Implemented a /stop endpoint to terminate all running worker threads and reset the system state.

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const http = require("http"); // To create web socket server
 const WebSocket = require("ws") // To real-time updates
-const cors = require("cors") // To resolve the sometimes browser blocking the request
+const cors = require("cors"); // To resolve the sometimes browser blocking the request
+const TicketPool = require("./ticketPool");
+const { Worker } = require("worker_threads");
 
 // initializing
 const app = express();
@@ -34,7 +36,7 @@ server_http.listen(PORT, () => {
     console.log(`Server is working on this url : http://localhost:${PORT}`)
 });
 
-const updateUser = (vendors) => {
+const updateUsers = (vendors) => {
     wss.clients.forEach((user) => {
         if (user.readyState === WebSocket.OPEN) {
             user.send(JSON.stringify(vendors));
@@ -53,5 +55,91 @@ const middleware = (req, res, next) => {
         next();
     }
 };
+
+// Initialize threads
+var threads = [];
+
+app.get("/", middleware, (req, res) => {
+    const maxTicketCapacity = req.query.maximumTicketCapacity;
+    const numberOfVendors = req.query.numOfVendors;
+    const numberOfCustomers = req.query.numOfCustomers;
+    const rateReleasing = req.query.rateOfTicketReleasing;
+    const rateRetrieving = req.query.rateOfCustomerRetrieval;
+    const totalTicketCount = req.query.totalTicketCount;
+
+    // Create a Shared TicketPool instance
+    const ticketPool = new TicketPool(        
+        maxTicketCapacity,
+        numberOfVendors,
+        numberOfCustomers,
+        updateUsers,
+        totalTicketCount
+    );
+
+    // Using a loop to create worker threads for each vendor
+    for (let i = 1; i <= numberOfVendors; i++) {
+        
+        // Create a new worker thread and passes vendor-data
+        const vendorWorker = new Worker("./vendor-ticket-worker.js", {
+            workerData: { vendorId: i, rateReleasing: rateReleasing },
+        });
+        threads.push(vendorWorker);
+
+        vendorWorker.on("message", async (notification) => {
+            if (notification.action === "generateTickets") {
+                await ticketPool.generateTickets(notification.vendorId);
+            }
+        });
+
+        vendorWorker.on("error", (error) => {
+            console.error(`Vendor-${i} encountered an error: `, error);
+        });
+
+        vendorWorker.on("exit", (exitCode) => {
+            if(exitCode !== 0) {
+                console.error(`Vendor-${i} has stopped with this exit code : ${exitCode}`);
+            }
+        });
+    }
+
+    // Using a loop to create worker threads for each customer
+    for (let i = 1; i <= numberOfCustomers; i++) {
+
+        // Create a new worker thread and passes customer-data
+        const customerWorker = new Worker("./customer-ticket-worker.js", {
+            workerData: { customerId: i, rateRetrieving: rateRetrieving },
+        });
+        threads.push(customerWorker);
+
+        customerWorker.on("message", async(notification) => {
+            if (notification.action === "reserveTickets") {
+                await ticketPool.purchaseTickets(notification.customerId);
+            }
+        });
+
+        customerWorker.on("error", (error) => {
+            console.error(`Customer-${i} encountered an error: `, error);
+        });
+
+        customerWorker.on("exit", (exitCode) => {
+            if(exitCode !== 0) {
+                console.error(`Customer-${i} has stopped with this exit code : ${exitCode}`);
+            }
+        });
+    }
+
+    res.send("System Excutes");
+});
+
+app.get("/stop", (req, res) => {
+    threads.forEach((thread) => {
+        thread.terminate().catch((error) => {
+            console.error("Error Occured. Failed to Terminate thread: ", error);
+        });
+    });
+    start = false;
+    threads = [];
+    res.send("All threads are terminated and system was stopped.")
+});
 
 
